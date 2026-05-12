266bbd4eca3cbd571b64bd1424f9c5e870c6f4d3
grifaello <grifaello@gmail.com>
2026-05-12 18:03:54 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* inicio da header*/}
+      <View style = {styles.viewHeader}>
+      <Feather name="menu" size ={24} color="black" />
+      <Text style = {styles.textHeader}> TECFILMES </Text>
+      <TouchableOpacity>
+      
+      </TouchableOpacity>
+      </View>
+
+  {/* inicio da barra de pesquisa*/}
+  <View style = {styles.containerSearch}>
+    <TextInput
+    placeholder='digite o filme que deseja buscar'
+      
+    ></TextInput>
+
+
+
+</View>
+
+    
+
+
+      
+      {/* inicio do banner*/}
+    
+    
+    
+    
+
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFFDD',
+    alignItems: "center",
   },
-});
+
+  viewHeader: {
+  flexDirection: 'row',
+  width: "90%",
+  alignItems: 'center',
+  marginTop: 10,
+  justifyContent:'space-between'
+},
+
+textHeader: {
+fontSize:25,
+color: 'black',
+fontWeight: 'bold',
+},
+
+containerSearch: {
+  marginTop:20,
+  width: "90%",
+  backgroundColor: 'white',
+},
+
+
+
+}
+)
