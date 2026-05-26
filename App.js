c8d73206c8c59ebd41697d3f1426c15923846425
grifaello <grifaello@gmail.com>
2026-05-26 17:55:01 -0300

    terceiro commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
 import Feather from '@expo/vector-icons/Feather';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
 
-      {/* inicio da header*/}
+                {/* Cabeçalho*/}
       <View style = {styles.viewHeader}>
       <Feather name="menu" size ={24} color="black" />
       <Text style = {styles.textHeader}> TECFILMES </Text>
@@ -13,24 +15,29 @@ export default function App() {
       </TouchableOpacity>
       </View>
 
-  {/* inicio da barra de pesquisa*/}
+
+                {/*Barra de pesquisa*/}
+
   <View style = {styles.containerSearch}>
     <TextInput
-    placeholder='digite o filme que deseja buscar'
-      
+    placeholder='Digite o filme que deseja buscar...'
+    style = {styles.inputSearch} 
     ></TextInput>
-
-
-
+   
+   <TouchableOpacity>
+    <Feather name="search" size ={24} color="black" />
+   </TouchableOpacity>
 </View>
 
     
 
 
       
-      {/* inicio do banner*/}
+                {/* Banner*/}
     
-    
+    <Text style = {styles.textBanner}> Em cartaz </Text>
+    <Image source ={require("./imagens/filme_1.jpg")}
+    style = {styles.imageBanner}></Image>
     
     
 
@@ -38,6 +45,8 @@ export default function App() {
     </View>
   );
 }
+
+                {/* Propriedades */}
 
 const styles = StyleSheet.create({
 
@@ -65,9 +74,33 @@ containerSearch: {
   marginTop:20,
   width: "90%",
   backgroundColor: 'white',
+  boderRadius:5,
+  padding:8,
+  flexDirection:'row',
+  justifyContent: 'space-between'
 },
 
+  inputSearch: {
+    height:40,
+    padding:8,
+    width:'100%'
 
+  },
 
+  imageBanner: {
+
+    width: '90%',
+    height: 200,
+    marginTop:15,
+    borderRadius:10
+  },
+
+  textBanner: {
+    color: 'black',
+    width: '90%',
+    fontSize: 30,
+    marginTop:15,
+    fontWeight: 'bold'
+  }
 }
 )
